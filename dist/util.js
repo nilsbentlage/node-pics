f3b16fc0b2becb7c1ff198e913b77cb8a16c4066
Nils Bentlage <nils.bentlage@gmail.com>
2023-03-22 01:20:52 +0100

    finished Class and used it

--- dist/util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function logThis(message) {
-    const timeStamp = new Date().toISOString().slice(2, 19).replace("T", " ");
-    console.log(`${timeStamp} | ` + message);
-}
-exports.default = logThis;
--- src/ImageRequest.ts
+++ src/ImageRequest.ts
@@ -1,5 +1,8 @@
+import log from "./util";
+import { Response } from "express";
 import fs from "fs";
-import path from "path";
+import path, { join } from "path";
+import sharp from "sharp";
 
 const imageFolder = "images";
 const cacheFolder = ".cache";
@@ -8,17 +11,17 @@ const picsRegex =
   /(?<basename>[A-z/\-][^_]*)(?:_)?(?<dimensions>\d{1,4}x\d{1,4})?(?:\.)(?<format>[a-z]{1,4})/;
 
 export class ImageRequest {
-  filename!: string;
+  requestedFile!: string;
   basename!: string;
   filePath?: string;
   dimensions: string;
   format!: string;
-  refs?;
+  files!: { original: any; cache: any; source: any };
 
   constructor(requested: string) {
     const regexArray = requested.match(picsRegex);
     if (regexArray) {
-      [this.filename, this.basename, this.dimensions, this.format] =
+      [this.requestedFile, this.basename, this.dimensions, this.format] =
         regexArray || new Array(4).fill(null);
       if (this.basename.includes("/")) {
         this.basename = this.basename.split("/").pop() || this.basename;
@@ -26,48 +29,79 @@ export class ImageRequest {
           .split("/", this.basename.split("/").length - 1)
           .join("/");
       }
-      this.refs = {
+      this.files = {
         original: {
-          path: path.join(imageFolder, this.filename),
-          exists: fs.existsSync(path.join(imageFolder, this.filename)),
+          path: path.join(imageFolder, this.requestedFile),
+          exists: fs.existsSync(path.join(imageFolder, this.requestedFile)),
           format: this.format,
         },
         cache: {
-          path: path.join(cacheFolder, this.filename),
-          exists: fs.existsSync(path.join(cacheFolder, this.filename)),
+          path: path.join(cacheFolder, this.requestedFile),
+          exists: fs.existsSync(path.join(cacheFolder, this.requestedFile)),
           format: this.format,
         },
         source: {
-          path: fs
-            .readdirSync(
-              path.join("images", this.filePath ? this.filePath : "")
-            )
-            .find((foundFile) => foundFile.startsWith(this.basename)),
+          path:
+            imageFolder +
+            "/" +
+            fs
+              .readdirSync(path.join("images", this.filePath || ""))
+              .find((foundFile) => foundFile.startsWith(this.basename)),
           exists: fs
-            .readdirSync(
-              path.join("images", this.filePath ? this.filePath : "")
-            )
+            .readdirSync(path.join("images", this.filePath || ""))
             .find((foundFile) => foundFile.startsWith(this.basename))
             ? true
             : false,
           format: fs
-            .readdirSync(
-              path.join("images", this.filePath ? this.filePath : "")
-            )
+            .readdirSync(path.join("images", this.filePath || ""))
             .find((foundFile) => foundFile.startsWith(this.basename))
             ?.split(".")[1],
         },
       };
-      console.log(this.refs);
     } else {
-      throw new Error("The requested filename does not meet our Requirements");
+      throw new Error(
+        "The requested requestedFile does not meet our Requirements"
+      );
+    }
+  }
+
+  convertImage(res?: Response) {
+    const sourceFile = this.files.source.path;
+    const sourceFileExtension = sourceFile.split(".")[1];
+    const [width, height] = this.dimensions.split("x");
+
+    return new Promise((resolve, reject) => {
+      try {
+        const image = sharp(join(sourceFile));
+
+        if (width && height) {
+          image.resize(parseInt(width), parseInt(height));
+        }
+        if (this.format !== sourceFileExtension) {
+          image.toFormat(this.format === "jpg" ? "jpeg" : (this.format as any));
+        }
+        image
+          .toFile(this.files.cache.path)
+          .then(() => {
+            res && res.sendFile(this.files.cache.path, { root: "." });
+          })
+          .then(() => {
+            resolve("done");
+          });
+      } catch (err: any) {
+        log("Could not create Image: " + err.message, res);
+        reject("error");
+      }
+    });
+  }
+
+  async serveImage(res?: Response) {
+    if (this.files.original.exists) {
+      res && res.sendFile(this.files.original.path, { root: "." });
+    } else if (this.files.cache.exists) {
+      res && res.sendFile(this.files.cache.path, { root: "." });
+    } else {
+      await this.convertImage(res);
     }
   }
 }
-
-console.log("Tests asdasd");
-new ImageRequest("test_300x300.jpg");
-new ImageRequest("test.jpg");
-new ImageRequest("models/test23123.jpg");
-new ImageRequest("tester/test2/test_231x231.jpg");
-new ImageRequest("tester/test2/test.png");
